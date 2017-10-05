02dc6d2348510ba1de377ee90342f452750003a3
Mike Dacanay <migd64@gmail.com>
2017-10-04 23:30:13 -0700

    look at solution

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
   validates :title, length: { minimum: 7}
   belongs_to :blog
   has_many :messages
+
+  before_destroy :destroy_messages
+
+  private
+
+  def destroy_messages
+  	self.messages.delete_all
+  end
 end
